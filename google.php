2c827794d56ad150ad8524bba5e683068b5e32d6
Tamra Pristou-Globokar <trp.globokar@knights.ucf.edu>
2012-04-22 10:00:49 -0400

    added seperate query and gtin conditionals depending on function

--- google.php
+++ google.php
@@ -1,9 +1,13 @@
 <?
 	$apiKey = "AIzaSyCQXpBJyooKMW-XI_fXgyCZZXpVZqqzTFA";
 	$query = $_GET['query'];
+	$gtin = $_GET['gtin'];
 	$query = urlencode($query);
 	
-	$url  = "https://www.googleapis.com/shopping/search/v1/public/products?key=$apiKey&country=US&language=en&currency=USD&q=$query";
+	$url  = "https://www.googleapis.com/shopping/search/v1/public/products?key=$apiKey&country=US&language=en&currency=USD";
+	
+	if(isset($_GET['query'])) $url .= "&q=$query";
+	else $url .= "&restrictBy=gtin=$gtin&rankBy=price:ascending&maxResults=1";
 	
 	echo file_get_contents($url);
 ?>

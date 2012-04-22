76fb04d6ba45fb70289270dcc851845cf317f0a6
thetindog <trp.globokar@knights.ucf.edu>
2012-04-22 10:35:56 -0300

    modified display search results and added "adds games" and "lists games"

--- script/script.js
+++ script/script.js
@@ -1,18 +1,72 @@
-$("#search").live("pageinit",function() {
+$("#myGames").live("pageinit",function() {
+
+	//adds games
+	var bindIt = function(){
+		$("#searchResults a").click(function() {
+			$.ajax({
+				url: "addgame.php?gtin=" + this.id,
+				success: function(data){
+					$("#searchResults").empty();				
+					$(document.createElement("li")).append(data).appendTo("#searchResults").trigger("create");
+					$("#searchResults").listview("refresh");
+				}
+				});
+		});
+	}
+	
+	//displays search results
 	$("#searchForm").submit(function(event) {
 		$.ajax({
-			url: "giantBomb.php?query=" + $("#searchBox").val(),
+			url: "google.php?query=" + $("#searchBox").val(),
 			dataType: 'json',
-			success: function(response) {
-				console.log(response);
+			success: function(data) {
 				$("#searchResults").empty();
-				$.each(response.results, function() {
-					console.log(this.image.icon_url);
-					$(document.createElement("li")).append("<img src=\""+this.image.icon_url+"\" />").append(this.name).appendTo("#searchResults").trigger("create");
+				var alreadyShown=new Array();
+				var tester = 0;
+				var gtinStr = "string";
+				$.each(data.items, function() {
+					gtinStr = this.product.gtin;
+					tester = $.inArray(gtinStr, alreadyShown);
+					if(tester == -1 && gtinStr != "undefined"){
+						alreadyShown.push(gtinStr);
+						$(document.createElement("li")).append("<a id=\""+gtinStr+"\" ><img src=\""+this.product.images[0].link+"\" />"+this.product.title+gtinStr+"</a>").appendTo("#searchResults").trigger("create");
+					}
 				});
 				$("#searchResults").listview("refresh");
+				bindIt();
 			}
 		});
 		return false;
 	});
+	
+	//lists games - right now I have this as click; if you can get it to work so it displays automatically, that'd be awesome
+	$('a').click(function() {
+		$.ajax({
+			url: "listgames.php",
+			dataType: "xml",
+			success: function(xml) {
+				var gameGTIN = "";
+				$("#gameListing").empty();	
+				$(xml).find('game').each(function(){
+					gameGTIN = $(this).text();
+					
+					$.ajax({
+						url: "google.php?gtin=" + gameGTIN,
+						dataType: 'json',
+						success: function(data){
+							var titleStr = "string";
+							var costStr = "cost";
+							$.each(data.items, function() {		
+							costStr = this.product.inventories[0].price;
+							$(document.createElement("li")).append("<a href=\"#vendorList\"><img width=\"50\" src=\""+this.product.images[0].link+"\" />"+this.product.title+ " $"+costStr+"</a>").appendTo("#gameListing").trigger("create");
+							});
+						}
+					});
+					
+				});
+				$("#gameListing").listview("refresh");
+			}
+		});
+	});
+	
 });

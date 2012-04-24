ef8d49e9d73436e25a6bd387904222a7285ce554
Tamra Pristou-Globokar <trp.globokar@knights.ucf.edu>
2012-04-24 00:07:45 -0400

    added functions to remove games so this is TECHNICALLY the EDITgames file but cest la vie

--- addgame.php
+++ addgame.php
@@ -3,14 +3,22 @@ session_start();
 include('db_connect.php');
 
 $gtin = $_GET['gtin'];
+$exgtin = $_GET['exgtin'];
+$outECHO = "";
 	
-$outECHO = "Please log in first.";
-
-if(isset($_SESSION['username'])){
-	$query = "insert into 4104_p3_listings (gtin, user_id) values (".$gtin.", ".$_SESSION['user_id'].")";
-	$result = $mysqli->query($query);
-	if($mysqli->error) $outECHO = "Query failed: ".$mysqli->error;
-	else $outECHO = "Your game has been added.";
+if(isset($_SESSION['user_id'])){
+	if(isset($_GET['gtin'])){
+		$query = "insert into 4104_p3_listings (gtin, user_id) values (".$gtin.", ".$_SESSION['user_id'].")";
+		$result = $mysqli->query($query);
+		if($mysqli->error) $outECHO = "Query failed: ".$mysqli->error;
+		else $outECHO = "Your game has been added.";
+	}
+	else if(isset($_GET['exgtin'])){
+		$query = "delete from 4104_p3_listings where user_id = '".$_SESSION['user_id']."' and gtin = ".$exgtin;
+		$result = $mysqli->query($query);
+		if($mysqli->error) $outECHO = "Query failed: ".$mysqli->error;
+		else $outECHO = "Your game has been deleted.";
+	}
 }
 
 echo $outECHO;

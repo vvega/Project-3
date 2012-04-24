d098efb2a6bc4953259b420cd79e65566bf21da3
Tamra Pristou-Globokar <trp.globokar@knights.ucf.edu>
2012-04-24 00:06:47 -0400

    added restricting criteria to only search for products from game stores

--- google.php
+++ google.php
@@ -7,7 +7,7 @@
 	
 	$url  = "https://www.googleapis.com/shopping/search/v1/public/products?key=$apiKey&country=US&language=en&currency=USD";
 	
-	if(isset($_GET['query'])) $url .= "&q=$query";
+	if(isset($_GET['query'])) $url .= "&restrictBy=(accountId=5620503|6432331|5701073|1224426|2462410|8536565)&crowdBy=gtin:1&q=$query";
 	else if(isset($_GET['gtin'])) $url .= "&restrictBy=gtin=$gtin&rankBy=price:ascending&maxResults=1";
 	else if(isset($_GET['vendorgtin'])) $url .= "&restrictBy=gtin=$vendorgtin&rankBy=price:ascending&maxResults=10";
 	
